c5aa046dbc6797c2d4e747d14c07c2b6b6a7c940
sxprr <magalajawad@gmail.com>
2023-06-24 15:43:23 +0100

    Add project files.

---
+++ ShopOnline.Api/Entities/cartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopOnline.Api.Entities
+{
+    public class cartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int productId { get; set; }
+        public int Qty { get; set; }
+    }
+}
